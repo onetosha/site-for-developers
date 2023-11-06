9e5ff26d87e0d20bfe31a4ff4ce453689b71ee39
onetosha <onetosha3@yandex.ru>
2023-11-06 14:37:01 +0500

    Налажены запросы к бэк-части приложения

--- src/main.js
+++ src/main.js
@@ -4,10 +4,6 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import { router } from './helpers';
 
-// setup fake backend
-import { fakeBackend } from './helpers';
-fakeBackend();
-
 const app = createApp(App);
 
 app.use(createPinia());
